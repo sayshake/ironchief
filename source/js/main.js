747e12656ed6f9ba3f7d1226c1aa1c10db9d69e9
Anton Ushakov <anton_ushakov@inbox.ru>
2017-12-28 14:36:13 +0300

    add all sections

---
+++ source/js/main.js
@@ -0,0 +1,12 @@
+(function(){
+    //add event listener for open form button
+    var openFormButton = document.querySelector('.arrow-down');
+    
+    if (openFormButton){
+        openFormButton.addEventListener('click',function(e){ 
+            e.preventDefault();
+            form.open();
+        })
+    }
+    
+}());
